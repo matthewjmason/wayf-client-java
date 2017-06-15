553814d463cf25ce84f0529d7966830885ce0f15
Matthew Mason <mjm@matthewjmason.com>
2017-06-15 16:06:34 -0400

    Signing fixes

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     openssl aes-256-cbc -K $encrypted_c759e9b053b2_key -iv $encrypted_c759e9b053b2_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-    gpg --fast-import cd/cosigningkey.asc
+    gpg --fast-import cd/codesigning.asc
 fi
